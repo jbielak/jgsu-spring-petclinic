e7ae5c9cad6dde87c0845688a2466ffc14373605
jbielak <justyna_bielak@outlook.com>
2020-11-29 08:08:27 -0800

    Copy pipeline out of jenkins web ui into a pipeline.groovy file to edit in vscode

---
+++ ide/pipeline.groovy
@@ -0,0 +1,26 @@
+pipeline {
+    agent any
+
+    stages {
+        stage('Checkout') {
+            steps {
+                // Get some code from a GitHub repository
+                git url: 'https://github.com/jbielak/jgsu-spring-petclinic.git', branch: 'main'
+            }
+        }
+        stage('Build') {
+            steps {
+                sh './mvnw clean package'
+            }
+
+            post {
+                // If Maven was able to run the tests, even if some of the test
+                // failed, record the test results and archive the jar file.
+                always  {
+                    junit '**/target/surefire-reports/TEST-*.xml'
+                    archiveArtifacts 'target/*.jar'
+                }
+            }
+        }
+    }
+}
